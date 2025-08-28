1be0e94e285439a28f373fd3c12b8ebd26c1e9e5
Jeffrin2005 <jeffrinjojo1@gmail.com>
2025-08-28 09:15:35 +0530

    adding

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
                 <div className="relative inline-block group">
                     <div className="absolute -inset-0.5 bg-neon_bg opacity-75 blur-sm rounded-xl group-hover:opacity-100 transition-opacity duration-300" />
                     <div className="absolute -inset-1 bg-neon_bg opacity-50 blur-md rounded-xl group-hover:opacity-75 transition-opacity duration-300" />
-                    <a href="https://drive.google.com/file/d/1txuQLeiLtPkhhPvbCRnXmw7ZPzIFaoSD/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="relative inline-flex items-center px-8 py-3 bg-black text-white border border-neon_bg rounded-xl space-x-2 hover:bg-neon_bg/10 transition-all duration-300" >
+                    <a href="https://drive.google.com/file/d/13OjBjPu6xMpDuxQRP2hRX6P21OdSL_vd/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="relative inline-flex items-center px-8 py-3 bg-black text-white border border-neon_bg rounded-xl space-x-2 hover:bg-neon_bg/10 transition-all duration-300" >
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" >
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                         </svg>
